7022ce1d313e47813fc6004864328d50fa91580a
Harvey Ji <harvey.ji123@gmail.com>
2024-12-09 20:38:03 -0600

    Humble beginnings

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,11 @@
+import "./NavBar.css";
+
+function NavBar() {
+    return(
+        <div className="navbarContainer">
+            <p>Navbar start</p>
+        </div>
+    )
+}
+
+export default NavBar;
