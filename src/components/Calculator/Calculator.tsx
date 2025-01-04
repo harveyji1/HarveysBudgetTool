e0a03e3eacfe048b47284cb0f447078dfefa62d4
Harvey Ji <harvey.ji123@gmail.com>
2025-01-03 22:31:56 -0600

    start of calculator

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -4,22 +4,18 @@ import Form from 'react-bootstrap/Form';
 
 function Calculator() {
     return(
-        <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+        <Form className='calculatorForm'>
+        <Form.Group className = "inputs">
+          <Form.Label>Income: </Form.Label>
+          <Form.Control />
         </Form.Group>
-  
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        {/* <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+        </Form.Group> */}
         <Button variant="primary" type="submit">
           Submit
         </Button>
