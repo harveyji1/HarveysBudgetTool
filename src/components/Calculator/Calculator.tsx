cf44ac51382fcba2f4db024bad85da360fe27a4d
Harvey Ji <harvey.ji123@gmail.com>
2025-01-08 17:06:27 -0600

    Made calculator more dynamic
    Made it possible to add custom fields in future

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,48 +1,69 @@
 import './Calculator.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
 
 function Calculator() {
-    return(
+  // Define categories and fields as data
+  const [categories, setCategories] = useState([
+    {
+      name: 'Income',
+      description: '',
+      fields: [
+        { label: 'Income (Total After Tax): ', value: '' },
+      ],
+    },
+    {
+      name: 'Fixed Expenses',
+      description: 'Expenses that can not or will not change',
+      fields: [
+        { label: 'Rent/Mortgage: ', value: '' },
+        { label: 'Car Payments: ', value: '' },
+        { label: 'Debt Payments: ', value: '' },
+        { label: 'Insurance: ', value: '' },
+        { label: 'Investments: ', value: '' },
+        { label: 'Tuition: ', value: '' },
+        { label: 'Emergency Funds: ', value: '' },
+      ],
+    },
+    {
+      name: 'Non-Fixed Expenses',
+      description: 'Expenses that you want to budget for',
+      fields: [],
+    },
+  ]);
 
-        <Form className='calculatorForm'> 
-          <div className='calculatorInputs'>
-            <h2 className='categoryHeader'>Income</h2>
-            <Form.Group className = "incomeInputs">
-              <Form.Label>Income (Total After Tax): </Form.Label>
-              <Form.Control type='number'/>
-            </Form.Group>
-            <h2 className='categoryHeader'>Fixed Expenses</h2>
-            <h4 className='categoryDesc'>Expenses that can not or will not change</h4>
-            <Form.Group className = "fixedExpenses">
-              <Form.Label>Rent/Mortgage: </Form.Label>
-              <Form.Control type='number'/>
-              <Form.Label>Car Payments: </Form.Label>
-              <Form.Control type='number'/>
-              <Form.Label>Debt Payments: </Form.Label>
-              <Form.Control type='number'/>
-              <Form.Label>Insurance: </Form.Label>
-              <Form.Control type='number'/>
-              <Form.Label>Investments: </Form.Label>
-              <Form.Control type='number'/>
-              <Form.Label>Tuiton: </Form.Label>
-              <Form.Control type='number'/>
-              <Form.Label>Emergency Funds: </Form.Label>
-              <Form.Control type='number'/>
+  const handleFieldChange = (categoryIndex: number, fieldIndex: number, value: string) => {
+    const updatedCategories = [...categories];
+    updatedCategories[categoryIndex].fields[fieldIndex].value = value;
+    setCategories(updatedCategories);
+  };
+
+  return (
+    <Form className='calculatorForm'>
+      <div className='calculatorInputs'>
+        {categories.map((category, categoryIndex) => (
+          <div key={categoryIndex} className='category'>
+            <h2 className='categoryHeader'>{category.name}</h2>
+            {category.description && <h4 className='categoryDesc'>{category.description}</h4>}
+            <Form.Group className='inputs'>
+              {category.fields.map((field, fieldIndex) => (
+                <div key={fieldIndex} className='formField'>
+                  <Form.Label>{field.label}</Form.Label>
+                  <Form.Control
+                    type='number'
+                    value={field.value}
+                    onChange={(e) => handleFieldChange(categoryIndex, fieldIndex, e.target.value)}
+                  />
+                </div>
+              ))}
             </Form.Group>
           </div>
-        {/* <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group> */}
-        <Button type="submit">
-          Calculate
-        </Button>
-      </Form>
-    )
+        ))}
+      </div>
+      <Button type="submit">Calculate</Button>
+    </Form>
+  );
 }
 
 export default Calculator;
