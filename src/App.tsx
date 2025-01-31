af4632ffdbecf8efcc9ac989450bf4e0ba12f9ba
Harvey Ji <harvey.ji123@gmail.com>
2025-01-31 15:40:03 -0600

    Site Title, AI Start

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 // import { useState } from 'react'
 import NavBar from './components/NavBar/NavBar';
 import Calculator from './components/Calculator/Calculator';
+import AISuggestions from './components/AISuggestions/AISuggestions';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
-  // const [count, setCount] = useState(0)
   return (
     <div>
       <NavBar/>
+      <h1 className='siteTitle'>Harveys AI Budget Tool</h1>
       <Calculator/>
+      <AISuggestions/>
     </div>
 
   )
---
+++ src/components/AISuggestions/AISuggestions.tsx
@@ -0,0 +1,12 @@
+import './AISuggestions.css';
+
+function AISuggestions(){
+    return(
+        <div id = "aiSuggestions">
+            <h1 className='aiHeader'>AI Functionality Coming Soon!</h1>
+        </div>
+        
+    )
+}
+
+export default AISuggestions;
